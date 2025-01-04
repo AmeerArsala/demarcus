e8438e972d09c04cbab214895fc298e9d12dac95
Ameer Arsala <ameer.arsala03@gmail.com>
2025-01-03 18:11:15 -0800

    vbump 2

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  format: ["esm"],
   dts: true,
   splitting: true,
   sourcemap: true,
   clean: true,
+  minify: true,
+  treeshake: true,
+  external: ["react", "react-dom"],
   outDir: "dist",
   target: "ES2020",
 });
